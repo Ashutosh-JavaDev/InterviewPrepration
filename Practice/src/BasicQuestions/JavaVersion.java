fb6ab8abfd640ac0c011cc9e8e4fcf1119f94298
Ashutosh Ayush <jbnashu297@gmail.com>
2025-06-30 21:48:27 +0530

    Java Version Class

--- Practice/src/BasicQuestions/JavaVersion.java
+++ Practice/src/BasicQuestions/JavaVersion.java
@@ -2,6 +2,9 @@ package BasicQuestions;
 
 public class JavaVersion {
     public static void main(String[] args) {
+        //Java Version
         System.out.println("\nJava Version : "+System.getProperty("java.version"));
+        // Java Runtime Version
+        System.out.println("Java Runtime Version : "+ System.getProperty("java.runtime.version"));
     }       
 }
