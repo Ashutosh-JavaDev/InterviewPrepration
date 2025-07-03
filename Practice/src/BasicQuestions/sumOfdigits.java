edd671cb96391c9f9d6e1fb1f45b3d4d8368cda2
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-03 16:24:07 +0530

    Sum of Digits

--- Practice/src/BasicQuestions/sumOfdigits.java
+++ Practice/src/BasicQuestions/sumOfdigits.java
@@ -1,5 +1,11 @@
 package BasicQuestions;
-
+import java.util.Scanner;
 public class sumOfdigits {
-    
+    int sum=0;
+  public int Sum(int number,int num){
+    // if(num%10)
+    sum+=number%10;
+    Sum(number/10,num);
+    return sum;
+  }  
 }
