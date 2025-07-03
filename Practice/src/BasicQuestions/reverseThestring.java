4541c97bdf684f98a12625ba2add5294935992c3
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-04 01:15:10 +0530

    Java Basic Questions

--- Practice/src/BasicQuestions/reverseThestring.java
+++ Practice/src/BasicQuestions/reverseThestring.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 public class reverseThestring {
    static String st="";
     public static String reverse(String str){
-        if(str.length()<=1){
+        if(str.length()==1){
             return str;
         }
         st+=str.charAt(str.length()-1);
       reverse(str.substring(0, str.length()-1));
       return st;
     }
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String");
+        String str=sc.nextLine();
+        String res=reverse(str);
+        System.out.println(res);
+    }
 }
