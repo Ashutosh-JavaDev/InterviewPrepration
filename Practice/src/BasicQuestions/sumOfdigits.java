8f4c63be0e400433cefd75a9429ccf5ec617bb92
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-04 00:55:10 +0530

    Java Basic Questions

--- Practice/src/BasicQuestions/sumOfdigits.java
+++ Practice/src/BasicQuestions/sumOfdigits.java
@@ -2,20 +2,17 @@ package BasicQuestions;
 import java.util.Scanner;
 public class sumOfdigits {
    static int sum=0;
-  public static int Sum(int number,int num){
+  public static int Sum(int num){
    if(num==0){
     return sum;
    }
-    sum+=num%10;
-    Sum(number, num/10);
-    return sum;
+    return (num%10)+Sum(num/10);
   }  
   public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter the number");
     int num=sc.nextInt();
-    int temp=num;
-    int res=Sum(num,temp);
+    int res=Sum(num);
     System.out.println(res);
   }
 }
