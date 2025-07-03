1e005a313b91b24e783f68d8087539a2d398d432
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-03 16:05:08 +0530

    Java Lang

--- Practice/src/BasicQuestions/JavaVersion.java
+++ Practice/src/BasicQuestions/JavaVersion.java
@@ -7,8 +7,13 @@ public class JavaVersion {
         // Java Runtime Version
         System.out.println("Java Runtime Version : "+ System.getProperty("java.runtime.version"));
         // Java URL Version
-        System.out.println("Java URL Version: "+System.getProperty("java.vendor.url"));
+        System.out.println("Java URL Version : "+System.getProperty("java.vendor.url"));
         // Java Vendor
         System.out.println("Java Vendor : "+System.getProperty("java.vendor"));
+        // Java Home
+        System.out.println("Java Home : "+System.getProperty("java.home"));
+        // Java class path
+        System.out.println("Java Class Path : "+System.getProperty("java.class.path"));
+        System.out.println();
     }       
 }
