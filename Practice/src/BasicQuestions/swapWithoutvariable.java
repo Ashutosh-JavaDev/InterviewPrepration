b25064d2f4c0362e2f0d0f3457dde4b0ec223572
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-05 01:05:31 +0530

    Java swap without variable

--- Practice/src/BasicQuestions/swapWithoutvariable.java
+++ Practice/src/BasicQuestions/swapWithoutvariable.java
@@ -12,4 +12,9 @@ public class swapWithoutvariable {
         System.out.println("After A: " + a);
         System.out.println("After B: " + b);
     }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two value");
+        swapping(sc.nextInt(), sc.nextInt());
+    }
 }
