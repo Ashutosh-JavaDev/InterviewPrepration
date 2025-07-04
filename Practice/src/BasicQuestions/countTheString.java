0a5ac91296167cfae16d8d28ec120c8294ff7dcb
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-04 11:22:58 +0530

    Java String Count

--- Practice/src/BasicQuestions/countTheString.java
+++ Practice/src/BasicQuestions/countTheString.java
@@ -2,8 +2,9 @@ package BasicQuestions;
 import java.util.Scanner;
 
 public class countTheString {
-    public static String count(String str){
-        
+    int count=0,space=0,number=0,special=0;
+    public static void Count(String str){
+
     }
     
 }
