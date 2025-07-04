35fbcf3de03ecbcd0aeea55d0f8a8ddbae023277
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-04 14:14:16 +0530

    Java String Count

--- Practice/src/BasicQuestions/countTheString.java
+++ Practice/src/BasicQuestions/countTheString.java
@@ -3,10 +3,27 @@ package BasicQuestions;
 import java.util.Scanner;
 
 public class countTheString {
-    int count = 0, space = 0, number = 0, special = 0;
+    static int count = 0, space = 0, number = 0, special = 0;
 
     public static void Count(String str) {
+        // if(str.length()==0){
+        //     return;
+        // }
+        for(char c:str.toCharArray()){
+            if(Character.isDigit(c)){
+                number++;
+            }
 
+            if(Character.isWhitespace(c)){
+                space++;
+            }
+            if(Character.isLetter(c)){
+                count++;
+            }
+            else{
+                special++;
+            }
+        }
     }
 
 }
