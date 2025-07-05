d48d92a4f5502e5102ef3e2a0b8497a9d5a915ff
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-06 01:14:06 +0530

    Java Prime Number

--- Practice/src/BasicQuestions/primeNumber.java
+++ Practice/src/BasicQuestions/primeNumber.java
@@ -1,13 +1,16 @@
 package BasicQuestions;
 
 public class primeNumber {
-    static boolean res=false;
-    public static boolean count(int num,int i){
-         if(num==1){
-            return true;
-         }
-         else if(num%i==0){
+    static boolean res = false;
+    static int value = 0;
 
-         }
+    public static boolean count(int num, int i) {
+        if (i == num) {
+            return true;
+        } else {
+            if (num % i == 0) {
+                value++;
+            }
+        }
     }
 }
