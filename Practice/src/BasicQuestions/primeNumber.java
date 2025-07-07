1ad4effa5a64bf3e411b3aa04fafdfda4e951120
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-07 16:34:10 +0530

    Java Prime Number

--- Practice/src/BasicQuestions/primeNumber.java
+++ Practice/src/BasicQuestions/primeNumber.java
@@ -7,7 +7,7 @@ public class primeNumber {
     static int value = 0;
 
     public static boolean count(int num, int i) {
-        if (i == num) {
+        if (i > num) {
             return res;
         } else {
             if (num % i == 0) {
@@ -30,7 +30,7 @@ public class primeNumber {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        boolean res = count(11, 1);
+        boolean res = count(21, 1);
         System.out.println("Result: " + res);
     }
 }
