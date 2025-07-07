489cd27aa94df3dc1baed8008c93e840c537b966
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-07 17:07:36 +0530

    Java Fibo Series

--- Practice/src/BasicQuestions/FibonacciSeries.java
+++ Practice/src/BasicQuestions/FibonacciSeries.java
@@ -1,7 +1,14 @@
 package BasicQuestions;
+
 import java.util.Scanner;
+
 public class FibonacciSeries {
-    public static void Sequence(int a,int b){
-        
+
+    public static void Sequence(int temp, int num, int a, int b) {
+        if (temp == num) {
+            return;
+        } else {
+
+        }
     }
 }
