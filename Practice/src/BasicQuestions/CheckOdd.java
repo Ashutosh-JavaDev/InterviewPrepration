84c4d88cbc4c5fc0ce0470b183eebb76fa793c08
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-11 10:43:15 +0530

    Java Odd List

--- Practice/src/BasicQuestions/CheckOdd.java
+++ Practice/src/BasicQuestions/CheckOdd.java
@@ -2,7 +2,6 @@ package BasicQuestions;
 import java.util.ArrayList;
 public class CheckOdd {
     static boolean res=false;
-    static int count=0;
     public static boolean check(ArrayList<Integer>list,int index){
         if(index==0){
             return res;
@@ -21,6 +20,13 @@ public class CheckOdd {
     }
     public static void main(String[]args){
         ArrayList<Integer>list=new ArrayList<>();
-        list.add(12);
+        list.add(11);
+        list.add(13);
+        list.add(15);
+        list.add(23);
+        boolean res=check(list, list.size()-1);
+        System.out.print(res);
+
+
     }
 }
