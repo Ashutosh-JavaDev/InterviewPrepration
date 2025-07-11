b7a919f20a9d855978d0d8d41e3759d6684eb634
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-12 02:39:02 +0530

    Java minimun Occurence

---
+++ Practice/src/BasicQuestions/minimumOccurence.java
@@ -0,0 +1,19 @@
+package BasicQuestions;
+
+public class minimumOccurence {
+    public static char Pass(String str){
+        int arr[]=new int [256];
+        for(int i=0;i<str.length()-1;i++){
+            arr[str.charAt(i)]+=1;
+        }
+        int min=-1;
+        char c=' ';
+        for(int i=0;i<str.length()-1;i++){
+            if(arr[str.charAt(i)]<min){
+                min=arr[str.charAt(i)];
+                c=str.charAt(i);
+            }
+        }
+        return c;
+    }
+}
