a7469d800905cfd7e7396d7c2cb35c3010231a97
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-11 22:30:21 +0530

    Java trim

--- Practice/src/BasicQuestions/removeleading.java
+++ Practice/src/BasicQuestions/removeleading.java
@@ -1,8 +1,14 @@
 package BasicQuestions;
-
+import java.util.Scanner;
 public class removeleading {
     public static String check(String str){
        return str.trim();
-       
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Sentence with leaving some extra Space at the Starting");
+        String str=sc.nextLine();
+        String st=check(str);
+        System.out.println(st);
     }
 }
