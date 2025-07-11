b83b7d150d92cd30cbafebbc2f6152a4a1225fa8
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-12 02:46:36 +0530

    Java Minimun

--- Practice/src/BasicQuestions/minimumOccurence.java
+++ Practice/src/BasicQuestions/minimumOccurence.java
@@ -1,24 +1,33 @@
 package BasicQuestions;
 
 public class minimumOccurence {
-    public static char Pass(String str){
-        int arr[]=new int [256];
-        for(int i=0;i<str.length()-1;i++){
-            arr[str.charAt(i)]+=1;
-        }
-        int min=-1;
-        char c=' ';
-        for(int i=0;i<str.length()-1;i++){
-            if(arr[str.charAt(i)]<min){
-                min=arr[str.charAt(i)];
-                c=str.charAt(i);
+    public static int Pass(int arr[],int size){
+        // int arr[]=new int [256];
+        // for(int i=0;i<str.length()-1;i++){
+        //     arr[str.charAt(i)]+=1;
+        // }
+        // int min=arr[0];
+        // char c=' ';
+        // for(int i=0;i<str.length()-1;i++){
+        //     if(min<arr[str.charAt(i)]){
+        //         min=arr[str.charAt(i)];
+        //         c=str.charAt(i);
+        //     }
+        // }
+        // return c;
+        int min=arr[0];
+            for(int i=0;i<size;i++){
+               for(int j=i+1;j<size;j++){
+                if(arr[i]>arr[j]){
+                    min=arr[i];
+                }
+               }
             }
-        }
-        return c;
+            return min;
     }
     public static void main(String[]args){
-        String str="RajaRam";
-        char ch=Pass(str);
-        System.out.println(ch);
+        String str="RaRRRjaRam";
+        // char ch=Pass(str);
+        // System.out.println(ch);
     }
 }
