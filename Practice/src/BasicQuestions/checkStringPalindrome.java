16ff9f76fb7bddf969462c22f836235d8c633bcc
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-11 13:08:24 +0530

    Java String Palindrome Check

--- Practice/src/BasicQuestions/checkStringPalindrome.java
+++ Practice/src/BasicQuestions/checkStringPalindrome.java
@@ -2,8 +2,21 @@ package BasicQuestions;
 
 public class checkStringPalindrome {
     static boolean res = false;
-
+    static String st="";
     public static boolean check(String str) {
-
+        if(str.length()==0){
+            return res;
+        }
+        else{
+            st+=str.charAt(str.length()-1);
+            check(str.substring(0, str.length()-1));
+        }
+        if(str.equals(st)){
+            res=true;
+        }
+        else{
+            res=false;
+        }
+        return res;
     }
 }
