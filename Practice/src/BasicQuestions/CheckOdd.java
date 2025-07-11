115ce674d93c00814703a73c7a3a4f5efe302373
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-11 11:08:24 +0530

    Java Odd List

--- Practice/src/BasicQuestions/CheckOdd.java
+++ Practice/src/BasicQuestions/CheckOdd.java
@@ -1,31 +1,32 @@
 package BasicQuestions;
+
 import java.util.ArrayList;
+
 public class CheckOdd {
-    static boolean res=false;
-    public static boolean check(ArrayList<Integer>list,int index){
-        if(index==0){
+    static boolean res = false;
+    public static boolean check(ArrayList<Integer> list, int index) {
+        if (index == 0) {
             return res;
-        }
-        else{
-            if(list.get(index)%2!=0){
-              res=true;
-                check(list,index-1);
-            }
-            else{
-                check(list, index-1);
-                return false;
+        } else {
+            if (list.get(index) % 2 != 0) {
+                res = true;
+                check(list, index - 1);
+            } else {
+                check(list, index - 1);
+                res=false;
             }
         }
         return res;
     }
-    public static void main(String[]args){
-        ArrayList<Integer>list=new ArrayList<>();
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
         list.add(11);
-        list.add(12);
-        list.add(15);
+        list.add(13);
+        list.add(17);
         list.add(23);
         System.out.println(list.size());
-        boolean res=check(list, list.size()-1);
+        boolean res = check(list, list.size() - 1);
         System.out.print(res);
         System.out.println();
 
