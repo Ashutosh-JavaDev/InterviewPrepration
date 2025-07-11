e01e6f4897e285e1743d7380dfcf4660532a0e8c
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-11 22:37:12 +0530

    Java Array Sort

---
+++ Practice/src/BasicQuestions/sortArray.java
@@ -0,0 +1,13 @@
+package BasicQuestions;
+import java.util.Arrays;
+public class sortArray {
+    public static void array(int arr[]){
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void main(String[]args){
+        int arr[]={21,4332,13,42,1323,122,23};
+        array(arr);
+
+    }
+}
