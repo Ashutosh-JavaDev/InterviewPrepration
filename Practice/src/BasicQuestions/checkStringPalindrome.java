5556669753f4c5764f866bca2a41fc8b96bb6e7e
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-11 13:10:02 +0530

    Java String Palindrome Check

--- Practice/src/BasicQuestions/checkStringPalindrome.java
+++ Practice/src/BasicQuestions/checkStringPalindrome.java
@@ -2,26 +2,26 @@ package BasicQuestions;
 
 public class checkStringPalindrome {
     static boolean res = false;
-    static String st="";
+    static String st = "";
+
     public static boolean check(String str) {
-        if(str.length()==0){
+        if (str.length() == 0) {
             return res;
+        } else {
+            st += str.charAt(str.length() - 1);
+            check(str.substring(0, str.length() - 1));
         }
-        else{
-            st+=str.charAt(str.length()-1);
-            check(str.substring(0, str.length()-1));
-        }
-        if(str.equalsIgnoreCase(st)){
-            res=true;
-        }
-        else{
-            res=false;
+        if (str.equalsIgnoreCase(st)) {
+            res = true;
+        } else {
+            res = false;
         }
         return res;
     }
-    public static void main(String[]args){
-        String str="racecar";
-        boolean res=check(str);
+
+    public static void main(String[] args) {
+        String str = "racecar";
+        boolean res = check(str);
         System.out.println(res);
     }
 }
