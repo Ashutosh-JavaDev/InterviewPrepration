0a4450262bbd61921fa7e3894317490f6c6f1831
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-12 02:56:37 +0530

    Java Factoria;

--- Practice/src/BasicQuestions/Factorial.java
+++ Practice/src/BasicQuestions/Factorial.java
@@ -1,5 +1,15 @@
 package BasicQuestions;
 
 public class Factorial {
-    
+    static int fact=1;
+    public static  int fact(int num){
+        if(num==1){
+            return fact;
+        }
+        else{
+            fact*=num;
+            fact(num-1);
+        }
+        return fact;
+    }
 }
