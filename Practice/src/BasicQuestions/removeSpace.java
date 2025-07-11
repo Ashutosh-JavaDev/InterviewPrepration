55ee17a46cb680bb13f2f0b657cc2af44346bcc2
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-11 13:29:32 +0530

    Java String Remove

--- Practice/src/BasicQuestions/removeSpace.java
+++ Practice/src/BasicQuestions/removeSpace.java
@@ -1,5 +1,10 @@
 package BasicQuestions;
 
 public class removeSpace {
-    
+    static boolean res=false;
+    public static  String remove(String  str){
+        if(str.length()-1==0){
+            return res;
+        }
+    }
 }
