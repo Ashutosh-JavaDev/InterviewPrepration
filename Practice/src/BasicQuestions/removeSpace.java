8d0a9059f20aacb77bbe57c4a90326ffdd5f98c5
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-11 13:35:58 +0530

    Java Remove String

--- Practice/src/BasicQuestions/removeSpace.java
+++ Practice/src/BasicQuestions/removeSpace.java
@@ -1,20 +1,28 @@
 package BasicQuestions;
 
 public class removeSpace {
-    
-    public static  String remove(String  str){
-        if(str.length()-1==0){
+
+    public static String remove(String str) {
+        if (str.length() - 1 == 0) {
             return str;
-        }
-        else{
-            str.trim();
-            remove(str.substring(0, str.length()-1));
+        } else {
+            if (str.contains(" ")) {
+                str.trim();
+                remove(str.substring(0, str.length() - 1));
+
+            }
+
+            else {
+                remove(str.substring(0, str.length() - 1));
+
+            }
         }
         return str;
     }
-    public static void main(String[]args){
-        String str="Jay Shree Ran";
-        String st=remove(str);
+
+    public static void main(String[] args) {
+        String str = "Jay Shree Ram";
+        String st = remove(str);
         System.out.println(st);
     }
 }
