7504bf8ff086cf618d5f7db75b80bc9ec03ee3b5
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-13 13:10:45 +0530

    Java Unique Qusstion

--- Practice/src/BasicQuestions/Unique.java
+++ Practice/src/BasicQuestions/Unique.java
@@ -1,6 +1,10 @@
 package BasicQuestions;
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.Set;
 public class Unique {
     public static void unique(int arr[]){
-        
+        String str[]={"wew","fer"};
+        HashSet<String>set=new HashSet<>(Arrays.asList(str));
     }    
 }
