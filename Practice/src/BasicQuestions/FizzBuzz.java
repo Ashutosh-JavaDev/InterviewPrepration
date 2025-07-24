10a49ab80b008e709a9e50ee9707dcc56ab7ffb6
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-24 16:39:19 +0530

    Java Fizz Buzz

---
+++ Practice/src/BasicQuestions/FizzBuzz.java
@@ -0,0 +1,10 @@
+package BasicQuestions;
+
+import java.util.ArrayList;
+
+public class FizzBuzz {
+    public static ArrayList<Integer> result(ArrayList<Integer> list) {
+        list=new ArrayList<>();
+        return list;
+    }
+}
