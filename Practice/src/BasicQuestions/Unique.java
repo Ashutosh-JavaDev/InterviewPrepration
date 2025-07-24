09a6646350e82300c0b0fc446cc56fe285b5c584
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-24 16:11:15 +0530

    Java Unique Qusstion

--- Practice/src/BasicQuestions/Unique.java
+++ Practice/src/BasicQuestions/Unique.java
@@ -6,8 +6,13 @@ import java.util.Set;
 import java.util.Scanner;
 public class Unique {
     public static ArrayList unique(HashSet<Integer>set){
+        
         ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<set.size();i++){
+            
+        }
         return list;
+
     }   
     public static void main(String[]args){
        
