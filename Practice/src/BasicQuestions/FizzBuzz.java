836c5aa12f5b6022272a7694055191dd942aa6ef
Ashutosh Ayush <jbnashu297@gmail.com>
2025-07-24 17:02:47 +0530

    Java Fizz Buzz

--- Practice/src/BasicQuestions/FizzBuzz.java
+++ Practice/src/BasicQuestions/FizzBuzz.java
@@ -3,13 +3,25 @@ package BasicQuestions;
 import java.util.ArrayList;
 
 public class FizzBuzz {
-    public static ArrayList<Integer> result(int n) {
-       ArrayList list=new ArrayList<>();
-        for(int i=0;i<list.size();i++){
-            if(list.get(i)%15==0){
-                System.out.println("FizzBuzz");
+    public static ArrayList<String> result(int n) {
+       ArrayList <String>list=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%15==0){
+               list.add("FizzBuzz");
+            }
+            else if(i%3==0){
+                list.add("Buzz");
+            }
+            else if(i%5==0){
+                list.add("Fizz");
+            }
+            else{
+                list.add(list.get(i));
             }
         }
         return list;
+    }
+    public static void main(String[] args) {
+        result(15);
     }
 }
